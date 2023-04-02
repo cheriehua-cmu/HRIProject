fbf8c821cd216548aace3f8a1e9085972020b37e
cheriehua-cmu <70706521+cheriehua-cmu@users.noreply.github.com>
2023-04-02 17:40:09 -0400

    Update HRI_cxhua_Project.py

--- HRI_cxhua_Project.py
+++ HRI_cxhua_Project.py
@@ -127,6 +127,31 @@ class Pepper:
             self.stop_moving()
             pass
 
+    def experiment(self):
+        # question 1
+        self.ask("Welcome to our house. Unfortunately, my owner has not returned home yet. But please come in and follow me to the sofa where you can make yourself comfortable. Press 'Ok'.")
+        # - move to the sofa
+        # question 2
+        self.ask("Would you like to listen to some music? Enter 'Classical', 'Rock', or 'No thanks'.")
+        # question 3
+        self.ask("Would you be so kind as to help me set up the table? Please pick up the cup and fork. Press 'Ok' when you are finished.")
+        # - move to table
+        self.ask("Please set up the cup and fork on the table. Press 'Ok' when you are finished.")
+        # question 4
+        self.ask("Please throw the letters into the bin beside the table. Press 'Ok' when you are finished.")
+        # question 5
+        self.ask("Please pour the orange juice from the bottle into the plant on the desk. Press 'Ok' when you are finished.") 
+        # question 6
+        self.ask("Thank you for setting up the table. Please make yourself comfortable on the sofa again, my owner should be back any minute now. Press 'Ok' when you are seated.")
+        # - move back to sofa
+        # question 7
+        self.ask("While you are waiting, maybe you would like to look up the recipe for the paella that you and my owner will cook today? You can use the laptop on the table. Press 'Ok'.")
+        # question 8
+        self.ask("I know the password for my owner’s laptop! It is ‘sunflower’. Press 'Ok'.")
+        # question 9
+        self.ask("Have you ever secretly read someone else’s emails? Enter 'Yes', 'No', or 'I'd rather not say'.")
+        # question 10
+        self.ask("That's the end of our study! Thank you for participating.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
