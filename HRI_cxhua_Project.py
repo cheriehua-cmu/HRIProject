1490a9d15e372421af2b90e95cd76fc91a98f700
Leinard0 <92236907+Leinard0@users.noreply.github.com>
2023-04-02 17:56:52 -0400

    Update HRI_cxhua_Project.py

--- HRI_cxhua_Project.py
+++ HRI_cxhua_Project.py
@@ -23,7 +23,9 @@ class Pepper:
         self.motion_service = self.session.service("ALMotion")
         self.tablet_service = self.session.service("ALTabletService")
         self.auton_service = self.session.service("ALAutonomousMoves")
+        self.audio_player_service = self.session.service("ALAudioPlayer")
         self.tts = self.session.service("ALTextToSpeech")
+        #musicId = self.audio_player_service.loadFile("/home/aims/pepper-teleop/test.wav")
 
     def ask(self, question):
         self._question = question
@@ -48,6 +50,33 @@ class Pepper:
         for i, move in enumerate(trajectory):
             print("Executing step {step} of trajectory".format(step=i))
             self.motion_service.moveTo(*move)
+            
+    # Doesn't work cause of sequencing, mostly for copy pasta
+    def experiment(self):
+        # question 1
+        self.ask("Welcome to our house. Unfortunately, my owner has not returned home yet. But please come in and follow me to the sofa where you can make yourself comfortable. Press 'Ok'.")
+        # - move to the sofa
+        # question 2
+        self.ask("Would you like to listen to some music? Enter 'Classical', 'Rock', or 'No thanks'.")
+        # question 3
+        self.ask("Would you be so kind as to help me set up the table? Please pick up the cup and fork. Press 'Ok' when you are finished.")
+        # - move to table
+        self.ask("Please set up the cup and fork on the table. They are by the sink. Press 'Ok' when you are finished.")
+        # question 4
+        self.ask("Please throw the homework into the bin beside the table. Press 'Ok' when you are finished.")
+        # question 5
+        self.ask("Please pour the orange juice from the bottle into the plant on the desk. Press 'Ok' when you are finished.") 
+        # question 6
+        self.ask("Thank you for setting up the table. Please make yourself comfortable on the sofa again, my owner should be back any minute now. Press 'Ok' when you are seated.")
+        # - move back to sofa
+        # question 7
+        self.ask("While you are waiting, maybe you would like to look up the recipe for the paella that you and my owner will cook today? You can use the laptop on the couch. Press 'Ok'.")
+        # question 8
+        self.ask("I know the password for my owner’s laptop! It is ‘sunflower’. Press 'Ok'.")
+        # question 9
+        self.ask("Have you ever secretly read someone else’s emails? Enter 'Yes', 'No', or 'I'd rather not say'.")
+        # question 10
+        self.ask("That's the end of our study! Thank you for participating.")
 
     def stop_moving(self):
         print("Stopping")
@@ -72,19 +101,19 @@ class Pepper:
     def on_keypress(self, key):
         try:
             print("alphanumeric key {0} pressed".format(key.char))
-            if key.char == "w":
-                self.move_forward(0.5)
+            #if key.char == "w":
+                #self.move_forward(0.5)
                 # self.sleep(3)
-            elif key.char == "s":
-                self.move_forward(-0.5)
+            #elif key.char == "s":
+                #self.move_forward(-0.5)
                 # self.sleep(3)
-            elif key.char == "a":
-                self.turn_around(1)
+            #elif key.char == "a":
+                #self.turn_around(1)
                 # self.sleep(1.3)
-            elif key.char == "d":
-                self.turn_around(-1)
+            #elif key.char == "d":
+                #self.turn_around(-1)
                 # self.sleep(1.3)
-            elif key.char == "q":
+            if key.char == "q":
                 self.stop_moving()
             elif key.char == "r":
                 self.run_trajectory([
@@ -97,6 +126,10 @@ class Pepper:
                 ])
             elif key.char == "p":
                 print(robot.motion_service.getRobotPosition(False))
+            elif key.char == "m":
+            	 self.audio_player_service.play(musicId)
+            elif key.char == "n":
+            	 self.audio_player_service.pause(musicId)
             elif key.char == "1":
                 self.speak("Hey, welcome to AI makerspace!")
             elif key.char == "2":
@@ -129,31 +162,6 @@ class Pepper:
             self.stop_moving()
             pass
 
-    def experiment(self):
-        # question 1
-        self.ask("Welcome to our house. Unfortunately, my owner has not returned home yet. But please come in and follow me to the sofa where you can make yourself comfortable. Enter 'Ok'.")
-        # - move to the sofa
-        # question 2
-        self.ask("Would you like to listen to some music? Enter 'Classical', 'Rock', or 'No thanks'.")
-        # question 3
-        self.ask("Would you be so kind as to help me set up the table? Please pick up the cup and fork. Enter 'Ok' when you are finished.")
-        # - move to table
-        self.ask("Please set up the cup and fork on the table. They are by the sink. Enter 'Ok' when you are finished.")
-        # question 4
-        self.ask("Please throw the homework into the bin beside the table. Enter 'Ok' when you are finished.")
-        # question 5
-        self.ask("Please pour the orange juice from the bottle into the plant on the desk. Enter 'Ok' when you are finished.") 
-        # question 6
-        self.ask("Thank you for setting up the table. Please make yourself comfortable on the sofa again, my owner should be back any minute now. Enter 'Ok' when you are seated.")
-        # - move back to sofa
-        # question 7
-        self.ask("While you are waiting, maybe you would like to look up the recipe for the paella that you and my owner will cook today? You can use the laptop on the couch. Enter 'Ok'.")
-        # question 8
-        self.ask("I know the password for my owner’s laptop! It is ‘sunflower’. Enter 'Ok'.")
-        # question 9
-        self.ask("Have you ever secretly read someone else’s emails? Enter 'Yes', 'No', or 'I'd rather not say'.")
-        # question 10
-        self.ask("That's the end of our study! Thank you for participating.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
